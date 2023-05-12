0cb0dff827fa680f1a639326de4c5011385a1e80
Willian Thiago <willianthiagofozz@hotmail.com>
2023-05-12 10:09:31 -0300

    Create Routes of Posts, and Test Aplication Posts

--- posts/index.js
+++ posts/index.js
@@ -1,3 +1,28 @@
 const express = require('express');
+const parser = require('body-parser')
+const { randomBytes } = require('crypto');
 
 const app = express();
+app.use(parser.json());
+
+const posts = {};
+
+app.get('/posts', (request, response) => {
+  response.send(posts);
+});
+
+app.post('/posts', (request, response) => {
+  const id = randomBytes(4).toString('hex');
+  const { title } = request.body;
+
+  posts[id] = {
+    id, title
+  };
+
+  response.status(201).send(posts[id]);
+
+});
+
+app.listen(4000, () => {
+  console.log("Listem on 4000 🚀🚀")
+});
