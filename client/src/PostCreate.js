d5245015bddf0f6828f193e17ecc299b2c9794d6
Willian Thiago <willianthiagofozz@hotmail.com>
2023-05-12 13:14:38 -0300

    Config CORS in services of posts and comments !!

---
+++ client/src/PostCreate.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import axios from 'axios';
+
+const PostCreate = () => {
+  const [title, setTitle] = useState('');
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    const response = await axios.post('http://localhost:4000/posts', {title});
+    console.log(response)
+
+    setTitle('');
+  }
+
+  return <div>
+    <form onSubmit={onSubmit}>
+      <div className="form-group">
+        <label>Title</label>
+        <input type="text" className="form-control" onChange={e => setTitle(e.target.value)} value={title} />
+      </div>
+      <button className="btn btn-primary">Submit</button>
+    </form>
+  </div>;
+};
+ 
+export default PostCreate;
